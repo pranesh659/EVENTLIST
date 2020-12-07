941b458c415965c3a45a34eb23e124a95b9bf751
praneshbk <59330065+praneshbk@users.noreply.github.com>
2020-12-07 18:34:14 +0530

    commit

--- routes/views/events.js
+++ routes/views/events.js
@@ -4,7 +4,7 @@ exports = module.exports=function(req,res){
     var view=new keystone.View(req,res);
     var locals=res.locals;
 
-    locals.section='event';
+    locals.section='events';
     locals.filters={
         events:req.params.events
     }
