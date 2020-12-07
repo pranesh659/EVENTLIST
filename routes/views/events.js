52403a74720950dbac3b3d1c1b63b71f2344c2fd
praneshbk <59330065+praneshbk@users.noreply.github.com>
2020-12-07 15:55:03 +0530

    first commit

---
+++ routes/views/events.js
@@ -0,0 +1,27 @@
+var keystone=require('keystone');
+
+exports = module.exports=function(req,res){
+    var view=new keystone.View(req,res);
+    var locals=res.locals;
+
+    locals.section='event';
+    locals.filters={
+        events:req.params.events
+    }
+    locals.data={
+        eventLists:[]
+    }
+
+    view.on('init', function(next){
+        var q=keystone.list('events').model.findOne({
+            slug:locals.filters.events
+
+        });
+        q.exec(function(err,result){
+            locals.data.events=result;
+            next(err);
+        });
+    });
+    
+    view.render('events');
+}
